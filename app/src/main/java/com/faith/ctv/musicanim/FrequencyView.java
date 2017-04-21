c4e6487771c4d9807670e50fd995719152721853
faith-hb <chxai1314@qq.com>
2017-04-21 17:20:37 +0800

    支持多屏幕适配

--- app/src/main/java/com/faith/ctv/musicanim/FrequencyView.java
+++ app/src/main/java/com/faith/ctv/musicanim/FrequencyView.java
@@ -19,9 +19,9 @@ public class FrequencyView extends View implements View.OnClickListener{
 
     private static final String TAG = "FrequencyView";
     private static final int LINE_ANIM_COUNT = 4; // 参与动画的竖线总数
-    private static final int SHORT_LINE_HEIGHT = 40; // 最短的线的高度
-    private static final int mLintToLineW = 20; // 线和线之间的距离值
-    private static final int FRAME_NUM = 8; // 每帧增加的高度
+    private int mShortLineHgt; // 最短的线的高度
+    private int mLintToLineW; // 线和线之间的距离值
+    private int mFrameNum; // 每帧增加的高度
     private static final int ANIM_DURTION = 120; // 动画刷新频率
     private Paint mPaint = new Paint();
     private int mStartX; // 线的起始横坐标
@@ -67,9 +67,9 @@ public class FrequencyView extends View implements View.OnClickListener{
         Log.d(TAG,"震动频率-->" + log);
     }
 
-    public static float dp2px(Context context, float dpValue) {
-        final float scale = context.getResources().getDisplayMetrics().density;
-        return dpValue * scale + 0.5f;
+    public int dp2px(float dpValue) {
+        final float scale = getContext().getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 
     private void init(){
@@ -77,63 +77,62 @@ public class FrequencyView extends View implements View.OnClickListener{
         mPaint.setAntiAlias(true); // 抗锯齿
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setDither(true);
-        mPaint.setStrokeWidth(dp2px(getContext(),2f)); // 画笔的粗细
+        mPaint.setStrokeWidth(dp2px(2f)); // 画笔的粗细
+
+        mLintToLineW = dp2px(10);
+        mFrameNum = dp2px(9);
+        mShortLineHgt = dp2px(20);
 
         setOnClickListener(this);
         initData();
     }
 
-    private void initData(){
-        post(new Runnable() {
-            @Override
-            public void run() {
-                logT("view的宽 = " + getWidth() + "-->view的高 = " + getHeight() + "-->leftPadding = " + getPaddingLeft() + "-->topPadding = "
-                        + getPaddingTop() + "-->rightPadding = " + getPaddingRight() + "-->bottomPadding = " + getPaddingBottom());
-                mStartX = getPaddingLeft();
-                mStartY = getPaddingTop();
+    private void initData() {
+        logT("view的宽 = " + getWidth() + "-->view的高 = " + getHeight() + "-->leftPadding = " + getPaddingLeft() + "-->topPadding = "
+                + getPaddingTop() + "-->rightPadding = " + getPaddingRight() + "-->bottomPadding = " + getPaddingBottom());
+        mStartX = getPaddingLeft();
+        mStartY = getPaddingTop();
 
-                List<Line> lines = new ArrayList<>();
-                mEndY = mStartY + SHORT_LINE_HEIGHT;
-                for (int i = 0; i < LINE_ANIM_COUNT; i++) { // 绘制首次的频率画面
-                    float startY = mEndY - SHORT_LINE_HEIGHT * (i + 1) * 1 / 2;
-                    float startX = mStartX + i * mLintToLineW;
-                    Line line = new Line(startX, startY, mEndY);
-                    lines.add(line);
-                    logT("测试y = " + startY);
-                }
+        List<Line> lines = new ArrayList<>();
+        mEndY = mStartY + mShortLineHgt;
+        for (int i = 0; i < LINE_ANIM_COUNT; i++) { // 绘制首次的频率画面
+            float startY = mEndY - mShortLineHgt * (i + 1) * 1 / 2;
+            float startX = mStartX + i * mLintToLineW;
+            Line line = new Line(startX, startY, mEndY);
+            lines.add(line);
+            logT("测试y = " + startY);
+        }
 
-                mMaxLineStartY = lines.get(3).startY;
+        mMaxLineStartY = lines.get(3).startY;
 
-                // 打乱集合顺序
-                Line line1 = new Line();
-                line1.x = lines.get(0).x;
-                line1.startY = lines.get(2).startY;
-                line1.startYs = new float[]{line1.startY + FRAME_NUM * 2, line1.startY+ FRAME_NUM, line1.startY }; // 由小到大
-                line1.stopY = lines.get(0).stopY;
-                mLines.add(line1);
+        // 打乱集合顺序
+        Line line1 = new Line();
+        line1.x = lines.get(0).x;
+        line1.startY = lines.get(2).startY;
+        line1.startYs = new float[]{line1.startY + mFrameNum * 2, line1.startY + mFrameNum, line1.startY}; // 由小到大
+        line1.stopY = lines.get(0).stopY;
+        mLines.add(line1);
 
-                Line line2 = new Line();
-                line2.x = lines.get(1).x;
-                line2.startY = lines.get(1).startY;
-                line2.startYs = new float[]{line2.startY - FRAME_NUM * 2, line2.startY - FRAME_NUM, line2.startY}; // 由大到小
-                line2.stopY = lines.get(1).stopY;
-                mLines.add(line2);
+        Line line2 = new Line();
+        line2.x = lines.get(1).x;
+        line2.startY = lines.get(1).startY;
+        line2.startYs = new float[]{line2.startY - mFrameNum * 2, line2.startY - mFrameNum, line2.startY}; // 由大到小
+        line2.stopY = lines.get(1).stopY;
+        mLines.add(line2);
 
-                Line line3 = new Line();
-                line3.x = lines.get(2).x;
-                line3.startY = lines.get(3).startY;
-                line3.startYs = new float[]{line3.startY + FRAME_NUM + FRAME_NUM * 1 / 2, line3.startY + FRAME_NUM + FRAME_NUM * 1 / 4, line3.startY}; // 由小到大
-                line3.stopY = lines.get(2).stopY;
-                mLines.add(line3);
+        Line line3 = new Line();
+        line3.x = lines.get(2).x;
+        line3.startY = lines.get(3).startY;
+        line3.startYs = new float[]{line3.startY + mFrameNum + mFrameNum * 1 / 2, line3.startY + mFrameNum + mFrameNum * 1 / 4, line3.startY}; // 由小到大
+        line3.stopY = lines.get(2).stopY;
+        mLines.add(line3);
 
-                Line line4 = new Line();
-                line4.x = lines.get(3).x;
-                line4.startY = lines.get(0).startY;
-                line4.startYs = new float[]{line4.startY - FRAME_NUM * 2, line4.startY - FRAME_NUM,  line4.startY}; // 由大到小
-                line4.stopY = lines.get(3).stopY;
-                mLines.add(line4);
-            }
-        });
+        Line line4 = new Line();
+        line4.x = lines.get(3).x;
+        line4.startY = lines.get(0).startY;
+        line4.startYs = new float[]{line4.startY - mFrameNum * 2, line4.startY - mFrameNum, line4.startY}; // 由大到小
+        line4.stopY = lines.get(3).stopY;
+        mLines.add(line4);
     }
 
     @Override
